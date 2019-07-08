feef60028d2d8773556d633ef0cb97956081c91e
Morgan Stanley <morganpstanley@gmail.com>
2019-07-08 04:54:43 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -18,6 +18,8 @@ def bonus
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
+   epic_tragedy[:capulet][:heroine][:status] = "dead"
+   epic_tragedy[:montague][:hero][:status] = "dead"
   }
 
   #code your solution here:
